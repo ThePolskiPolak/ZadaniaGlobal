2719c9f1c434a618fd8aeacbb8c3f161a971ed26
ThePolskiPolak <domi152522@gmail.com>
2022-12-06 16:05:37 +0000

    dodany skrypt check_target.sh

---
+++ Zadania przykladowe/check_target.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+TARGET=192.168.56.123
+
+if [[ "$1" != "" ]];
+then
+    TARGET="$1"
+fi
+
+ping $TARGET -c 1 -t1 >/dev/null 2>&1
+res=$?
+
+if [ "$res" == 1 ]; then
+    echo "Target is not reachable"
+else
+    echo "Target is present"
+fi
+
+exit $res
